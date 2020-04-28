14c3c26b72ca8f6562e5c185ca05c6482fd59307
Noon <noonw8713@gmail.com>
2020-04-28 05:09:05 -0500

    Update

--- README.md
+++ README.md
@@ -20,6 +20,9 @@ All bugs and issues should be refered to the common-bugs.txt either here in this
 
 ## Usage
 ```
+- To run, open the "Noon's Video Downloader" Shortcut.
+- Or, you can run the "Video Downloader.exe" as admin found in your install location (Default: C:\Program Files (x86)\Noon\Noons Video Downloader\)
+
 All you have to do is input the url in the top textboxt then select an ouput. 
 
 There are other options like cookies for privated links, and format options like MP3 (Only current format). 
--- Video Downloader/MainWindow.xaml.cs
+++ Video Downloader/MainWindow.xaml.cs
@@ -130,29 +130,6 @@ namespace Video_Downloader
             InitializeCFG();
         }
 
-        private void AdminRelauncher()
-        {
-            if (!IsRunAsAdmin())
-            {
-                ProcessStartInfo proc = new ProcessStartInfo();
-                proc.UseShellExecute = true;
-                proc.WorkingDirectory = Environment.CurrentDirectory;
-                proc.FileName = Assembly.GetEntryAssembly().CodeBase;
-
-                proc.Verb = "runas";
-
-                try
-                {
-                    Process.Start(proc);
-                    Application.Current.Shutdown();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("This program must be run as an administrator! \n\n" + ex.ToString());
-                }
-            }
-        }
-
         private bool IsRunAsAdmin()
         {
             WindowsIdentity id = WindowsIdentity.GetCurrent();
